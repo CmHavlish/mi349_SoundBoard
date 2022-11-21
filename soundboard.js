136cf2a743047b16f2391570184bd876c14dd4fc
CmHavlish <cameronhavlish@gmail.com>
2022-11-20 22:14:30 -0500

    all the stuff

---
+++ soundboard.js
@@ -0,0 +1,15 @@
+const sounds = ['arrow', 'cannon', 'cuckoo'];
+
+sounds.forEach(sound => {
+    const btn = document.createElement('button')
+    btn.classList.add('btn');
+
+    btn.innerText = sound;
+
+     btn.addEventListener('click', () => {
+        document.getElementById(sound).play();
+    });
+
+    document.getElementById('buttons').appendChild(btn);
+    
+});
